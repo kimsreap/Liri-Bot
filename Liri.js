663bd3a34d12d6a56b5ceb8daf7190ec14fc005e
Kimsreang Phan <kimsreangphan@Kimsreangs-MacBook-Pro.local>
2019-04-25 23:46:29 -0700

    error:(

--- Liri.js
+++ Liri.js
@@ -1,14 +1,22 @@
+const dotenv = require("dotenv").config();
+var Spotify = require('node-spotify-api');
+const Twitter = require('Twitter');
+var keys = require("./keys.js");
+var request = require('request');
+const fs = require('fs');
 const axios = require('axios')
 
 
 axios.get('htpp://www.omdbapi.com/?t=Roma&apikey=trilogy')
-.then(({ data: {Title, Year, Rated, Runtime, Director} }) => {
+.then(({ data: {Title, Year, Rated, Runtime, Director, Language, Country} }) => {
     console.log(`
         Title: ${Title}
         Year: ${Year}
         Rated: ${Rated}
         Runtime: ${Runtime}
         Director: ${Director}
+        Language: ${Language}
+        Country: ${Country}
     `)
 })
 .catch(e => console.log(e))
