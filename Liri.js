9f537cf1ddcb6cc11b6453c33aa6b313168c191b
Kimsreang Phan <kimsreangphan@Kimsreangs-MacBook-Pro.local>
2019-04-28 09:23:02 -0700

    Not sure how to do it

--- Liri.js
+++ Liri.js
@@ -1,22 +1,63 @@
+
+    
 const dotenv = require("dotenv").config();
 var Spotify = require('node-spotify-api');
 const Twitter = require('Twitter');
 var keys = require("./keys.js");
 var request = require('request');
-const fs = require('fs');
-const axios = require('axios')
+
+var client = new Twitter(keys.twitterKeys);
+const {appendFile,readFile} = require('fs');
+const [, ,command,search] = process.argv;
+
+const doThis = _ => {
+    readFile('random.txt', 'utf8', (e, data) => {
+      if (e){  
+        console.log(e)
+    } else {
+        let doIt = data.split(', ')
+        doIt.pop()
+        console.log(doIt)
+    }
+    })
+}
 
 
-axios.get('htpp://www.omdbapi.com/?t=Roma&apikey=trilogy')
-.then(({ data: {Title, Year, Rated, Runtime, Director, Language, Country} }) => {
+switch (command) {
+	case 'my-tweets':
+	twitter(userSearch);
+	break;
+
+	case 'spotify-this-song':
+	spotify(userSearch);
+	break;
+
+	case 'movie-this':
+	movie();
+	break;
+
+	case 'do-what-it-says':
+	doThis ();
+	break;
+};
+
+
+const axios = require('axios');
+const [, , movie] = process.argv
+axios.get(`htpp://www.omdbapi.com/?t=${movie}&apikey=trilogy`)
+.then(({ data: {Title, Year, Rated, Actors, Plot, Language, Country,imdbRating} }) => {
     console.log(`
-        Title: ${Title}
-        Year: ${Year}
-        Rated: ${Rated}
-        Runtime: ${Runtime}
-        Director: ${Director}
-        Language: ${Language}
-        Country: ${Country}
+        Title of the movie: ${Title}
+        Year the movie came out: ${Year}
+        Rotten Tomato Rating: ${Rated}
+        Actors in the movie: ${Actors}
+        Plot of the movie: ${Plot}
+        Language of the movie: ${Language}
+        Country where the movie was produced: ${Country}
+        IMDB Rating of the movie: ${imdbRating}
+
     `)
 })
 .catch(e => console.log(e))
+
+
